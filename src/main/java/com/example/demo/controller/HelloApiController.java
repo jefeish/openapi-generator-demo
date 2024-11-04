711dd6ee29ab57fba0ae006bbc13d428046b6107
Jürgen Efeish <jefeish@github.com>
2024-11-04 12:57:48 -0500

    Create HelloApiController.java

---
+++ src/main/java/com/example/demo/controller/HelloApiController.java
@@ -0,0 +1,62 @@
+package com.example.demo.controller;
+
+import org.openapitools.model.HelloGet200Response;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.example.demo.api.ApiUtil;
+import com.example.demo.api.HelloApi;
+
+import org.springframework.web.context.request.NativeWebRequest;
+
+import javax.validation.constraints.*;
+import javax.validation.Valid;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import javax.annotation.Generated;
+
+@Controller
+@RequestMapping("${openapi.hello.base-path:/api/v1}")
+public class HelloApiController implements HelloApi {
+
+    private final NativeWebRequest request;
+
+    @Autowired
+    public HelloApiController(NativeWebRequest request) {
+        this.request = request;
+    }
+
+    @Override
+    public Optional<NativeWebRequest> getRequest() {
+        return Optional.ofNullable(request);
+    }
+
+    public ResponseEntity<HelloGet200Response> helloGet() {
+        getRequest().ifPresent(request -> {
+            for (MediaType mediaType: MediaType.parseMediaTypes(request.getHeader("Accept"))) {
+                if (mediaType.isCompatibleWith(MediaType.valueOf("application/json"))) {
+                    String exampleString = "{ \"message\" : \"Hello, World!\" }";
+                    ApiUtil.setExampleResponse(request, "application/json", exampleString);
+                    break;
+                }
+            }
+        });
+        return new ResponseEntity<>(HttpStatus.OK);
+
+    }
+}
